f693ff46f18091746aa3f0cb100f1ed7519822ec
Coleton Gorecke <coletoncodes@gmail.com>
2023-05-17 20:49:39 -0500

    Added Environment Schemes and seperate databases

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,31 +7,10 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
-     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
     // Setup Database
-//    app.databases.use(
-//        .postgres(
-//            configuration:
-//                    .init(
-//                        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-//                        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-//                        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-//                        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-//                        database: Environment.get("DATABASE_NAME") ?? "financeflow",
-//                        tls: .prefer(try .init(configuration: .clientDefault)))
-//        ), as: .psql)
-    
-    // Setup Database
-    app.databases.use(
-        .postgres(
-            hostname: "localhost",
-            username: "cgorecke",
-            password: "",
-            database: "financeflow"
-        ),
-        as: .psql
-    )
+    try configureDatabase(for: app)
     
     app.migrations.add(CreateUser())
     app.migrations.add(CreateUserToken())
@@ -40,4 +19,50 @@ public func configure(_ app: Application) async throws {
     
     // register routes
     try routes(app)
+}
+
+fileprivate func configureDatabase(for app: Application) throws {
+    let configuration: SQLPostgresConfiguration
+    switch app.environment {
+    case .testing:
+        configuration = SQLPostgresConfiguration(
+            hostname: "localhost",
+            username: "vapor_username",
+            password: "vapor_password",
+            database: "financeflow_test",
+            tls: .prefer(try .init(configuration: .clientDefault))
+        )
+    case .development:
+        configuration = SQLPostgresConfiguration(
+            hostname: "localhost",
+            username: "vapor_username",
+            password: "vapor_password",
+            database: "financeflow_dev",
+            tls: .prefer(try .init(configuration: .clientDefault))
+        )
+    case .production:
+        guard
+            let hostname = Environment.get("DATABASE_HOST"),
+            let portString = Environment.get("DATABASE_PORT"),
+            let port = Int(portString),
+            let username = Environment.get("DATABASE_USERNAME"),
+            let password = Environment.get("DATABASE_PASSWORD"),
+            let database = Environment.get("DATABASE_NAME")
+        else {
+            app.logger.error("Cannot run Production locally. Set the DATABASE_URL environment variable, or use Development scheme")
+            throw Abort(.internalServerError)
+        }
+        configuration = SQLPostgresConfiguration(
+            hostname: hostname,
+            port: port,
+            username: username,
+            password: password,
+            database: database,
+            tls: .prefer(try .init(configuration: .clientDefault))
+        )
+    default:
+        throw Abort(.internalServerError)
+    }
+    
+    app.databases.use(.postgres(configuration: configuration), as: .psql)
 }
