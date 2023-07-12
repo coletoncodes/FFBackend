f110f92fa424ef062b13e08afbcd91022afada9d
Coleton Gorecke <coletoncodes@gmail.com>
2023-07-11 22:32:20 -0500

    Added initial BudgetNetworkService

---
+++ Sources/FFAPI/Sources/FFAPI/Networking/FFBudgetNetworkService.swift
@@ -0,0 +1,48 @@
+//
+//  FFBudgetNetworkService.swift
+//  
+//
+//  Created by Coleton Gorecke on 7/11/23.
+//
+
+import Foundation
+
+public protocol FFBudgetNetworkService {
+    func getBudgetCategories(body: FFGetBudgetCategoriesRequestBody, accessToken: FFAccessToken, refreshToken: FFRefreshToken) async throws -> [FFBudgetCategory]
+    func getBudgetItems(body: FFGetBudgetItemsRequestBody, accessToken: FFAccessToken, refreshToken: FFRefreshToken) async throws -> [FFBudgetItem]
+}
+
+public final class FFBudgetNetworkingService: FFBudgetNetworkService, FFNetworkService {
+    
+    // MARK: - Initializer
+    public init() {}
+    
+    // MARK: - Interface
+    public func getBudgetCategories(
+        body: FFGetBudgetCategoriesRequestBody,
+        accessToken: FFAccessToken,
+        refreshToken: FFRefreshToken
+    ) async throws -> [FFBudgetCategory] {
+        return try await performRequest(
+            FFGetBudgetCategoriesRequest(
+                body: body,
+                refreshToken: refreshToken,
+                accessToken: accessToken
+            )
+        )
+    }
+    
+    public func getBudgetItems(
+        body: FFGetBudgetItemsRequestBody,
+        accessToken: FFAccessToken,
+        refreshToken: FFRefreshToken
+    ) async throws -> [FFBudgetItem] {
+        return try await performRequest(
+            FFGetBudgetItemsRequest(
+                body: body,
+                refreshToken: refreshToken,
+                accessToken: accessToken
+            )
+        )
+    }
+}
