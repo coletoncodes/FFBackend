16cdad92d0b2a61c43f85596d77b2c2deda9d0d5
Coleton Gorecke <coletoncodes@gmail.com>
2023-07-25 20:46:26 -0500

    Added BudgetItemNetworkingService

---
+++ Sources/FFAPI/Sources/FFAPI/Networking/FFBudgetItemNetworkService.swift
@@ -0,0 +1,36 @@
+//
+//  FFBudgetItemNetworkService.swift
+//  
+//
+//  Created by Coleton Gorecke on 7/25/23.
+//
+
+import Foundation
+
+public protocol FFBudgetItemNetworkService {
+    func saveItem(body: FFBudgetItemRequestBody) async throws
+    func deleteItem(body: FFBudgetItemRequestBody) async throws
+}
+
+public final class FFBudgetItemNetworkingService: FFBudgetItemNetworkService, FFNetworkService {
+    // MARK: - Properties
+    private let accessToken: FFAccessToken
+    private let refreshToken: FFRefreshToken
+    
+    // MARK: - Initializer
+    public init(
+        accessToken: FFAccessToken,
+        refreshToken: FFRefreshToken
+    ) {
+        self.accessToken = accessToken
+        self.refreshToken = refreshToken
+    }
+    
+    public func saveItem(body: FFBudgetItemRequestBody) async throws {
+        try await performRequest(FFPostBudgetItemRequest(body: body, accessToken: accessToken, refreshToken: refreshToken))
+    }
+    
+    public func deleteItem(body: FFBudgetItemRequestBody) async throws {
+        try await performRequest(FFDeleteBudgetItemRequest(body: body, accessToken: accessToken, refreshToken: refreshToken))
+    }
+}
