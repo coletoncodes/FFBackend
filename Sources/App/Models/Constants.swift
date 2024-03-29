8a475808e18f3be083077908812c56c75b07df3a
Coleton Gorecke <coletoncodes@gmail.com>
2024-03-29 13:38:21 -0500

    Removed constants

--- Sources/App/Models/Constants.swift
+++ Sources/App/Models/Constants.swift
@@ -8,12 +8,8 @@
 import Foundation
 
 @frozen enum Constants: String {
-    // TODO: Move to environment
     // sandbox
-    case plaidBaseURL = "https://sandbox.plaid.com"
-    case plaidClientId = "644d45b175067100187e30eb"
-    case plaidSecretKey = "787992f3ee35e6df430a4fd1f28446"
-    // development
-//    case plaidSecretKey = "e32beb193a5ee09bd20a814b04ef4a"
-//    case plaidBaseURL = "https://development.plaid.com"
+    case plaidBaseURL = "DUMMY URL"
+    case plaidClientId = "DUMMY ID"
+    case plaidSecretKey = "DUMMY SECRET"
 }
